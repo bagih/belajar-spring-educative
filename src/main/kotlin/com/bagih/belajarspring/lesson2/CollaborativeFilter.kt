caa1249e72125d67c930130020524586881a9330
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-06 22:06:02 +0800

    initial commit

--- src/main/kotlin/com/bagih/belajarspring/lesson2/CollaborativeFilter.kt
+++ src/main/kotlin/com/bagih/belajarspring/lesson2/CollaborativeFilter.kt
@@ -1,5 +1,6 @@
 package com.bagih.belajarspring.lesson2
 
+//main
 class CollaborativeFilter: Filter {
     override fun getRecommendation(movie: String): List<String>{
         return listOf("tenggelamnya kafal van der wijck", "titanic")
