d393b5dbcdbb446836663d0e89887bab1e4ff6ae
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-09 21:55:15 +0800

    lesson 8 finished: Bean scope

--- src/main/kotlin/com/bagih/belajarspring/BelajarspringApplication.kt
+++ src/main/kotlin/com/bagih/belajarspring/BelajarspringApplication.kt
@@ -1,5 +1,7 @@
 package com.bagih.belajarspring
 
+import com.bagih.belajarspring.lesson3.CollaborativeFilter
+import com.bagih.belajarspring.lesson3.ContentBasedFilter
 import com.bagih.belajarspring.lesson3.RecommenderImpl
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -11,6 +13,27 @@ fun main(args: Array<String>) {
 	val context = runApplication<BelajarspringApplication>(*args)
 
 	val recommenderImpl = context.getBean(RecommenderImpl::class.java)
+
+//	collaborative filter use prototype scope
+	val cf1 = context.getBean(CollaborativeFilter::class.java)
+	val cf2 = context.getBean(CollaborativeFilter::class.java)
+	val cf3 = context.getBean(CollaborativeFilter::class.java)
+
+//	contentBased filter uses singleton scope
+	val cb1 = context.getBean(ContentBasedFilter::class.java)
+	val cb2 = context.getBean(ContentBasedFilter::class.java)
+	val cb3 = context.getBean(ContentBasedFilter::class.java)
+
+//	print all collaborative filter bean instance
+	println(cf1)
+	println(cf2)
+	println(cf3)
+
+//	print all contentBased filter instance
+	println(cb1)
+	println(cb2)
+	println(cb3)
+
 	val results = recommenderImpl.recommendedMovies("KKN Desa Penari")
 	println(results.toString())
 }
--- src/main/kotlin/com/bagih/belajarspring/lesson3/CollaborativeFilter.kt
+++ src/main/kotlin/com/bagih/belajarspring/lesson3/CollaborativeFilter.kt
@@ -1,10 +1,13 @@
 package com.bagih.belajarspring.lesson3
 
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
 @Component
 @Qualifier("CF")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 class CollaborativeFilter: Filter {
     override fun getRecommendation(movie: String): List<String>{
         return listOf("tenggelamnya kafal van der wijck", "titanic")
